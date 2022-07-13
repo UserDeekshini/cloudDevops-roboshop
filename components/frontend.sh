8051ec462728e530b894656e834df017fbf946b6
Kukkunoor Gopal <254207@cognizant.com>
2022-07-13 16:27:05 +0530

    changes

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-echo "hello from frontend"
+#The frontend is the service in RobotShop to serve the web content over Nginx.
+
+set -e 
+
+#user validation 
+USER_ID=$(id -u)
+if[$USER_ID ne 0];then
+    echo "\e[32m User is not root!!! nginx installation is not possible \e[0m"
+    exit 1
+fi
+#Installing Nginx.
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+
+```
+
+Let's download the HTDOCS content and deploy it under the Nginx path.
+
+```
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+```
+
+Deploy in Nginx Default Location.
+
+```
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+```
