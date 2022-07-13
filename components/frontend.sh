9d057d3f4344ebe0302f514d113be0db4302d302
Kukkunoor Gopal <254207@cognizant.com>
2022-07-13 17:03:13 +0530

    changes

--- components/frontend.sh
+++ components/frontend.sh
@@ -10,12 +10,12 @@ LOGFILE="/temp/$COMPONENT.log"
 #user validation 
 USER_VALIDATION
 
-echo "Installing Nginx : "
-yum install nginx -y  &>> $LOGFILE
+echo -n "Installing Nginx : "
+yum install nginx -y &>> $LOGFILE
 if [ $? -eq 0 ] ; then
     echo -e "\e[32m SUCCESS \e[0m"
 else
-    echo -e "\e[32m FAILURE \e[0m"
+    echo -e "\e[32m FAILURE \e[0m"  
     
 #systemctl enable nginx
 #systemctl start nginx
