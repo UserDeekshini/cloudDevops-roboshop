54843b34d4a25c78897dad34ed09ca66d7c3018e
Kukkunoor Gopal <254207@cognizant.com>
2022-07-13 17:21:04 +0530

    changes

--- components/frontend.sh
+++ components/frontend.sh
@@ -7,27 +7,47 @@ COMPONENT=frontend
 LOGFILE="/tmp/$COMPONENT.log"
 source components/common.sh
 
-
-
+stat()
+{
+if [ $1 -eq 0 ] ;  then
+    echo -e "\e[32m SUCCESS \e[0m"
+else
+    echo -e "\e[32m FAILURE \e[0m"  
+fi
+}
 #user validation 
 USER_VALIDATION
 
 echo -n "Installing Nginx : "
 yum install nginx -y  &>>  $LOGFILE
-if [ $? -eq 0 ] ;  then
-    echo -e "\e[32m SUCCESS \e[0m"
-else
-    echo -e "\e[32m FAILURE \e[0m"  
-fi
-    
-#systemctl enable nginx
-#systemctl start nginx
-#curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-#Deploy in Nginx Default Location.
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "Enabling Nginx : "
+systemctl enable nginx  &>>$LOGFILE
+stat $?
+
+echo -n "Strating Nginx : "
+systemctl start nginx &>>  $LOGFILE
+stat $?
+
+echo -n "Downloading $COMPONENT : "
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" 
+stat $?
+
+
+echo -n "Clearing contents from Nginx default location : "
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
+
+echo -n "Extracting the $COMPONENT : "
+unzip /tmp/$COMPONENT.zip
+stat $?
+
+echo -n "Updating the proxy file : "
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+stat #?
