a53c5892d20ec6e67531293773c70976836f92e2
Kukkunoor Gopal <254207@cognizant.com>
2022-07-14 11:16:32 +0530

    changes

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -3,51 +3,14 @@
 set -e
 COMPONENT=catalogue
 LOG_FILE="/tmp/$COMPONENT.log"
-NODE_SOURCE="https://rpm.nodesource.com/setup_lts.x"
 APPUSER="roboshop"
 
 #user validation 
 source components/common.sh
 
-echo -n "Installing nodeJS"
-curl -sL $NODE_SOURCE | bash &>>$LOG_FILE
-yum install nodejs -y  &>>$LOG_FILE
-stat $?
+#calling function from common.sh 
+NODEJS()
 
-
-echo -n "Creating the roboshop user : "
-id roboshop &>>$LOG_FILE || adduser roboshop &>>$LOG_FILE
-stat $?
-
-
-echo -n "Downloading the $COMPONENT repo : "
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
-
-echo -n "Performing cleanup : "
-cd /home/roboshop && rm -rf ${COMPONENT} &>>$LOG_FILE
-stat $?
-
-echo -n "Extracting the ${COMPONENT} : "
-unzip -o /tmp/${COMPONENT}.zip &>>$LOG_FILE
-mv ${COMPONENT}-main ${COMPONENT} &&    chown -R $APPUSER:$APPUSER $COMPONENT
-cd /home/roboshop/$COMPONENT 
-stat $?
-
-echo -n "Installing  the $COMPONENT"
-npm install &>> $LOG_FILE
-stat $?
-
-
-echo -n "Updating SystemD file with service name : "
-sed -i -e 's/MONGO_DNSNAME/mongodb.rhobode.iternal/' /home/roboshop/catalogue/systemd.service
-cat /home/$APPUSER/$COMPONENT/systemd.service &>>$LOG_FILE
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $?
-
-echo -n "Starting the $COMPONENT : "
-START()
-stat $?
 
 #In order to make it work, update the proxy file in Nginx with the `CATALOGUE` server IP Address in the **`FRONTEND`** Server  
 #**`Note:`** Do not do a copy and paster of IP in the proxy file, there are high chances to enter the empty space characters, which are not visible on the vim editor. Manual Typing of IP Address/ DNS Name is preferred. 
--- components/common.sh
+++ components/common.sh
@@ -16,9 +16,68 @@ else
 fi
 }
 
-START(){
-systemctl daemon-reload 
-systemctl restart $COMPONENT 
-systemctl enable $COMPONENT 
-systemctl status $COMPONENT -l 
+NODEJS(){
+    echo -n "Configuring nodeJS Repos : "
+    curl -sL "https://rpm.nodesource.com/setup_lts.x" | bash &>>$LOG_FILE
+    stat $?
+
+    echo -n "Installing nodeJS"
+    yum install nodejs -y  &>>$LOG_FILE
+    stat $?
+
+    #calling user creation function 
+    CREATE_USER()
+
+    #calling function to download and extract the component files
+    DOWNLOAD_AND_EXTRACT()
+
+    echo -n "Installing  the $COMPONENT"
+    npm install &>> $LOG_FILE
+    stat $?
+    
+    #calling function to configure the component services
+    CONFIG_SERVICE()
+
+    #calling function start service
+    START_SERVICE()
+
+}
+
+CREATE_USER(){
+    echo -n "Creating the roboshop user : "
+    id roboshop &>>$LOG_FILE || adduser roboshop &>>$LOG_FILE
+    stat $?
+}
+
+DOWNLOAD_AND_EXTRACT(){
+    echo -n "Downloading the $COMPONENT repo : "
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $?
+
+    echo -n "Performing cleanup : "
+    cd /home/roboshop && rm -rf ${COMPONENT} &>>$LOG_FILE
+    stat $?
+
+    echo -n "Extracting the ${COMPONENT} : "
+    unzip -o /tmp/${COMPONENT}.zip &>>$LOG_FILE
+    mv ${COMPONENT}-main ${COMPONENT} &&    chown -R $APPUSER:$APPUSER $COMPONENT
+    cd /home/roboshop/$COMPONENT 
+    stat $?
+}
+
+CONFIG_SERVICE(){
+    echo -n "Updating SystemD file with service name : "
+    sed -i -e 's/MONGO_DNSNAME/mongodb.rhobode.iternal/' /home/roboshop/catalogue/systemd.service
+    cat /home/$APPUSER/$COMPONENT/systemd.service &>>$LOG_FILE
+    mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+    stat $?
+}
+
+START_SERVICE(){
+    echo -n "Starting the $COMPONENT service : "
+    systemctl daemon-reload 
+    systemctl restart $COMPONENT 
+    systemctl enable $COMPONENT &>> $LOG_FILE
+    systemctl status $COMPONENT -l &>> $LOG_FILE
+    stat $?
 }
