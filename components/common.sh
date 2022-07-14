139b8302b91c5f5cacb84f4d89681740a661ec22
Kukkunoor Gopal <254207@cognizant.com>
2022-07-14 12:42:51 +0530

    changes

--- components/common.sh
+++ components/common.sh
@@ -67,8 +67,7 @@ DOWNLOAD_AND_EXTRACT(){
 
 CONFIG_SERVICE(){
     echo -n "Updating SystemD file with service name : "
-    sed -i -e 's/REDIS_ENDPOINT/redis.rhobode.iternal/' /home/roboshop/$COMPONENT/systemd.service
-    sed -i -e 's/MONGO_ENDPOINT/mongodb.rhobode.iternal/' /home/roboshop/$COMPONENT/systemd.service 
+    sed -i -e 's/REDIS_ENDPOINT/redis.rhobode.iternal/' -e 's/MONGO_ENDPOINT/mongodb.rhobode.iternal/' /home/roboshop/$COMPONENT/systemd.service
     cat /home/$APPUSER/$COMPONENT/systemd.service &>>$LOG_FILE
     mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
     stat $?
