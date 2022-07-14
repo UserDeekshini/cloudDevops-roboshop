ec0275d67242d9d89c8f28e88c82ff676afcde0c
Kukkunoor Gopal <254207@cognizant.com>
2022-07-14 08:30:30 +0530

    changes

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -20,43 +20,40 @@ stat $?
 
 
 echo -n "Downloading the $COMPONENT repo : "
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
 echo -n "Performing cleanup : "
-cd /home/roboshop && rm -rf $COMPONENT &>>$LOG_FILE
+cd /home/roboshop && rm -rf ${COMPONENT} &>>$LOG_FILE
 stat $?
 
-echo -n "Extracting the $COMPONENT : "
-unzip -o /tmp/$COMPONENT.zip &>>$LOG_FILE
-mv $COMPONENT-main $COMPONENT
+echo -n "Extracting the ${COMPONENT} : "
+unzip -o /tmp/${COMPONENT}.zip &>>$LOG_FILE
+mv ${COMPONENT}-main ${COMPONENT}
+cd /home/roboshop/$COMPONENT 
 stat $?
 
 echo -n "Installing  the $COMPONENT"
-cd /home/roboshop/$COMPONENT 
-npm install & >>$LOG_FILE
+npm install & >> $LOG_FILE
 stat $?
 
-echo -n "Updating SystemD file with correct IP addresses : "
-sed -i -e 's/MONGO_DNSNAME/172.31.11.146/' /home/roboshop/catalogue/systemd.service
-cat /home/roboshop/$COMPONENT/systemd.service &>>$LOG_FILE
-mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $?
 
-echo -n "Starting the $COMPONENT : "
-systemctl daemon-reload &>>$LOG_FILE
-systemctl restart $COMPONENT &>>$LOG_FILE
-systemctl enable $COMPONENT &>>$LOG_FILE
-systemctl status $COMPONENT -l &>>$LOG_FILE
+# echo -n "Updating SystemD file with correct IP addresses : "
+# sed -i -e 's/MONGO_DNSNAME/172.31.11.146/' /home/roboshop/catalogue/systemd.service
+# cat /home/roboshop/$COMPONENT/systemd.service &>>$LOG_FILE
+# mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+# stat $?
+
+# echo -n "Starting the $COMPONENT : "
+# systemctl daemon-reload &>>$LOG_FILE
+# systemctl restart $COMPONENT &>>$LOG_FILE
+# systemctl enable $COMPONENT &>>$LOG_FILE
+# systemctl status $COMPONENT -l &>>$LOG_FILE
 
 #Now, you would still see **`CATEGORIES`** on the frontend page as empty. 
 #That’s because your `frontend` doesn't know who the `CATALOGUE` is when someone clicks the `CATEGORIES` option. So, we need to update the Nginx Reverse Proxy on the frontend. If not, you’d still see the frontend without categories.
-
 #In order to make it work, update the proxy file in Nginx with the `CATALOGUE` server IP Address in the **`FRONTEND`** Server  
-
 #**`Note:`** Do not do a copy and paster of IP in the proxy file, there are high chances to enter the empty space characters, which are not visible on the vim editor. Manual Typing of IP Address/ DNS Name is preferred. 
 # vim /etc/nginx/default.d/roboshop.conf
-
 #1. Reload and restart the Nginx service.
-
 # systemctl restart nginx
