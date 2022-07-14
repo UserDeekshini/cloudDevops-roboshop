489f7ecdef60447efe3f036c05b7ab1ae911267b
Kukkunoor Gopal <254207@cognizant.com>
2022-07-14 09:01:03 +0530

    changes

--- components/redis.sh
+++ components/redis.sh
@@ -10,8 +10,10 @@ source components/common.sh
 echo -e "Installing Redis : "
 curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 yum install redis-6.2.7 -y  &>> $LOG_FILE
+stat $?
 
 echo -n "Updating $COMPONENT Listening address : "
 sed -i -e "s/127.0.0.1/0.0.0.0/"  /etc/redis.conf
 sed -i -e "s/127.0.0.1/0.0.0.0/"  /etc/redis/redis.conf
 cat /etc/redis/redis.conf &>> $LOG_FILE
+stat $?
