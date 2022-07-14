a4c448edc4c9de2934727d88ac9a42c8cf2292f9
Kukkunoor Gopal <254207@cognizant.com>
2022-07-14 08:33:26 +0530

    changes

--- components/common.sh
+++ components/common.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-USER_VALIDATION(){
 if [ $USER_ID -ne 0 ]; then
     echo -e "\e[31m you need to be a root user to install software!!! \e[0m"
     exit 1
 fi
-}
+
 
 stat()
 {
