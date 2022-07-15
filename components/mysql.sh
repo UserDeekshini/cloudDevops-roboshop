fd6f67858fbd28ddf7801bb32462c7509f96abf6
Kukkunoor Gopal <254207@cognizant.com>
2022-07-15 20:31:23 +0530

    changes

--- components/mysql.sh
+++ components/mysql.sh
@@ -23,8 +23,8 @@ stat $?
 
 
 echo -n "Changing the Default $COMPONENT username and password : "
-echo "show databases" | mysql -uroot -pRoboShop@1
-if [$? -ne 0]; then
+echo "show databases" | mysql -uroot -pRoboShop@1 &>>$LOG_FILE
+if [ $? -ne 0 ]; then
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');"  > /tmp/rootpassword_change.sql
 DEFAULT_MYSQLROOT_PASSWORD=$(sudo grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
 mysql --connect-expired-password -uroot -p"$DEFAULT_MYSQLROOT_PASSWORD"  < /tmp/rootpassword_change.sql
