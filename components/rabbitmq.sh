05172dc3a14a27cbd910645412ef5720ed008b92
Kukkunoor Gopal <254207@cognizant.com>
2022-07-19 11:25:58 +0530

    changes

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -7,12 +7,12 @@ LOG_FILE="/tmp/$COMPONENT.log"
 #user validation 
 source components/common.sh
 
-echo -n "Installing dependency for $COMPONENT : "
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  
+echo -n "Setingup YUM repositories for $COMPONENT : "
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOG_FILE
 stat $?
 
-echo -n "Setingup YUM repositories for $COMPONENT : "
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash 
+echo -n "Installing dependency for $COMPONENT : "
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> $LOG_FILE
 stat $?
 
 echo -n "Installing $COMPONENT : "
